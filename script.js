06452eabc053e07f43b4d3051a8ff83d8c54a184
Asfak Ahmed <asfakahmed680@gmail.com>
2023-10-10 10:30:09 +0600

    problem solved by asfak ahmed

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 // DOM and html conector
 
-const Title = document.getElementById('Title')
-const Author = document.getElementById('Author')
-const Year = document.getElementById('Year')
-const addBtn = document.getElementById('addBtn')
-const bookList = document.getElementById('book-list')
+const Title = document.getElementById("Title");
+const Author = document.getElementById("Author");
+const Year = document.getElementById("Year");
+const addBtn = document.getElementById("addBtn");
+const bookList = document.getElementById("book-list");
 
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  // Basic Validation
+  if (Title.value !== "" && Author.value !== "" && Year.value !== "") {
+    let NewRow = document.createElement("tr");
+    NewRow.classList.add("bg-green-100");
 
+    // Creating new title
+    let NewTitle = document.createElement("th");
+    NewTitle.innerHTML = Title.value;
+    NewRow.appendChild(NewTitle);
 
-addBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
+    // Creating new title
+    let NewAuthor = document.createElement("th");
+    NewAuthor.innerHTML = Author.value;
+    NewRow.appendChild(NewAuthor);
+    // Creating new title
+    let NewYear = document.createElement("th");
+    NewYear.innerHTML = Year.value;
+    NewRow.appendChild(NewYear);
 
-    // Basic Validation
-    if(Title.value == '' && Author.value == '' && Year.value == ''){
-        alert('Please input your information.');
-    }else{
-        let NewRow = document.createElement('tr');
-        NewRow.classList.add('bg-green-100');
-
-        // Creating new title
-        let NewTitle = document.createElement('th');
-        NewTitle.innerHTML = Title.value;
-        NewRow.appendChild(NewTitle);
-
-        // Creating new title
-        let NewAuthor = document.createElement('th');
-        NewAuthor.innerHTML = Author.value;
-        NewRow.appendChild(NewAuthor);
-        // Creating new title
-        let NewYear = document.createElement('th');
-        NewYear.innerHTML = Year.value;
-        NewRow.appendChild(NewYear);
-
-        // Displaying in UI
-        bookList.appendChild(NewRow);
-}
-    Title.value = '';
-    Author.value = '';
-    Year.value = '';
-
-
-})
+    // Displaying in UI
+    bookList.appendChild(NewRow);
+  } else {
+    alert("Please input your information.");
+  }
+  Title.value = "";
+  Author.value = "";
+  Year.value = "";
+});
